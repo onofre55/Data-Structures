6238e8f2d7ef3253e3af9a72897b36a9208300fe
Javier Onofre <142274921+onofre55@users.noreply.github.com>
2025-02-16 15:38:15 -0500

    4. Stack

---
+++ Stack/Stack.cpp
@@ -0,0 +1,59 @@
+﻿/*
+Author: Adrian Onofre
+Purpose: To test the stack functionalities
+
+*/
+
+#include "Stack.hpp"
+
+int main()
+{
+
+    //Creating a Stack object with a given capacity
+
+    Stack s(5);
+
+    //Checking stack size
+
+    std::cout << s.size() << std::endl;
+
+    //Checking if the stack is empty
+
+    std::cout << "Is the stack empty?" << (s.isEmpty() ? " Yes " : " No ") << std::endl;
+
+    //Pushing values into the stack
+
+    s.push(32);
+
+    s.push(5);
+
+    s.push(10);
+
+    s.push(12);
+
+    s.push(15);
+
+    //Checking if the stack is full
+
+    std::cout << "Is the stack full?" << (s.isFull() ? " Yes " : " No ") << std::endl;
+
+    //Print the stack
+
+    s.print();
+
+    //Peeking at the elements
+
+    std::cout << s.peek() << std::endl;
+
+    std::cout << s.pop() << std::endl;
+
+    std::cout << s.peek() << std::endl;
+
+    std::cout << s.pop() << std::endl;
+
+    std::cout << s.peek() << std::endl;
+
+    std::cout << s.pop() << std::endl;
+
+}
+
---
+++ Stack/Stack.hpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+/*
+Author: Adrian Onofre
+Purpose: To write a stack and its functionalities.
+
+Write these operations for the class:
+ destructor
+ size
+ empty
+ full
+ push
+ pop
+ peek
+ print
+
+*/
+
+class Stack {
+
+private:
+
+	int* array;
+	int top;
+	int capacity;
+
+
+public:
+
+	//Constructor
+
+	Stack(int size) : top(-1), capacity(size), array(new int[size]) {}
+
+	//Destructor
+
+	~Stack(){
+
+		delete[] array;
+
+	}
+
+	//Operations
+
+	int size() {
+
+		return top + 1;
+
+	}
+
+	bool isEmpty() {
+
+		return top == -1;
+
+	}
+
+	bool isFull() {
+
+		return top == capacity - 1;
+
+	}
+
+	void push(int value) {
+
+		if (top == capacity - 1) {
+
+			std::cout << ("Stack is full!");
+
+		}
+
+		array[++top] = value;
+
+	}
+
+	int pop() {
+
+		if (isEmpty()) {
+
+			std::cout << ("Stack is empty!");
+
+		}
+
+		return array[top--];
+
+	}
+
+	int peek() {
+
+		if (isEmpty()) {
+
+			std::cout << "Stack is empty!";
+			return -1;
+
+		}
+
+		return array[top];
+
+	}
+
+	void print() {
+
+		if (isEmpty()) {
+
+			std::cout << "Stack is empty!";
+			return;
+
+		}
+
+		for (int i = top; i >= 0; i--) {
+
+			std::cout << array[i] << " , ";
+
+		}
+
+		std::cout << std::endl;
+
+	}
+};
